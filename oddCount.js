4517365446c78b2aa676484f520d821dc9674fd1
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-09 18:28:31 -0500

    Create oddCount.js

---
+++ oddCount.js
@@ -0,0 +1,23 @@
+function oddCount(n){
+ 
+  return Math.floor(n / 2);
+}
+
+//////////////////////////////
+
+
+NOT OPTIMIZED VERSION
+
+function oddCount(n){
+ var count = [];
+ 
+ for (var i=1; i<n; i++){
+    if(i %2!==0){
+      count.push(i)
+    
+  }
+  }
+  
+  return count.length;
+}
+
