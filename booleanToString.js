588fca66d0447dc3f3fca230fe7d23adc0550a5d
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-09 18:50:37 -0500

    Create booleanToString.js

---
+++ booleanToString.js
@@ -0,0 +1,22 @@
+function booleanToString(b){
+return b.toString();
+}
+
+/////////////////////////////
+
+function booleanToString(b){
+  return `${b}`
+}
+
+////////////////////////////
+
+function booleanToString(b){
+  if (typeof b == 'boolean') {
+    return String(b);
+  }
+}
+
+/////////////////////////////
+
+
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/solutions/javascript
