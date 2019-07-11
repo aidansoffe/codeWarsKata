c89e604923f576b1cdd98679c3f6d16f599a01e2
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-10 20:27:21 -0500

    Create combat.js

---
+++ combat.js
@@ -0,0 +1,13 @@
+function combat(health, damage) {
+   let newHealth = health-damage;
+ if(newHealth <= 0){
+   return 0;
+ }else{
+ return newHealth;
+ }
+}
+
+////////////////////////////////
+
+
+https://www.codewars.com/kata/grasshopper-terminal-game-combat-function-1/train/javascript
