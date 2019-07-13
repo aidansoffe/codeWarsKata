9ab86da76693de36be60b40139e97ec9b09864b4
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-12 22:33:47 -0500

    Create  squareSum.js

---
+++ squareSum.js
@@ -0,0 +1,23 @@
+function squareSum(array){
+ var sum = 0
+     for (let i of array){
+     sum += i*i
+     }
+  return sum;
+}
+
+//////////////////////////
+
+function squareSum(array){
+ var sum = 0 
+     for(var i=0; i<array.length;i++){
+   sum += Math.pow(array[i], 2);
+   }
+  return sum;
+}
+
+//////////////////////////////
+
+
+https://www.codewars.com/kata/square-n-sum/train/javascript
+
