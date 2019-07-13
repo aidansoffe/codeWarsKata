bdca2a7744dfbaee1bac7377984704a7ee502c0a
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-12 22:17:53 -0500

    Create positiveSum.js

---
+++ positiveSum.js
@@ -0,0 +1,13 @@
+function positiveSum(s) {
+var sum = 0;
+for (var i=0; i<arr.length; i++){
+if(arr[i]>0){
+sum += arr[i]
+}
+}
+return sum
+}
+
+/////////////////////////////
+
+https://www.codewars.com/kata/sum-of-positive/train/javascript
