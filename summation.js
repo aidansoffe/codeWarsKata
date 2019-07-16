08052225cb6c3b8d216aaa0c78710c651d0657c6
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-16 17:36:03 -0500

    Create summation.js

---
+++ summation.js
@@ -0,0 +1,11 @@
+var summation = function (n) {
+  var r = 0;
+  for(var i=1; i<=n; i++){
+  r = r + i
+}
+return r
+}
+
+//////////////////
+
+https://www.codewars.com/kata/grasshopper-summation/train/javascript
