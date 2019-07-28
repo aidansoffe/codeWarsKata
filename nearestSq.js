faef0c9c9fa9bb988f3b397cba433024747de3b6
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-28 00:17:13 -0400

    Create nearestSq.js

---
+++ nearestSq.js
@@ -0,0 +1,13 @@
+function nearestSq(n){
+   return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+
+//////////////////////////
+
+function nearestSq(n){
+    return Math.round(n**0.5)**2
+}
+//////////////////////////
+
+
+https://www.codewars.com/kata/find-nearest-square-number/train/javascript
