ec4cdb56f131ebbb873ffb53d964d2b496edb619
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-30 18:54:08 -0400

    Create getVolumeOfCuboid.js

---
+++ getVolumeOfCuboid.js
@@ -0,0 +1,16 @@
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length*width*height
+  };
+
+  return Kata;
+
+})();
+
+//////////////////
+
+https://www.codewars.com/kata/volume-of-a-cuboid/train/javascript
