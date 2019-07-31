36f8f31296b82819d54d98117666659db041fcbe
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-31 17:06:27 -0400

    Create twiceAsOld.js

---
+++ twiceAsOld.js
@@ -0,0 +1,15 @@
+function twiceAsOld(d, s) {
+  // your code here
+  return Math.abs(d=s*2-d)
+}
+
+/////////////////////////
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+////////////////////////
+
+
+https://www.codewars.com/kata/twice-as-old/train/javascript
