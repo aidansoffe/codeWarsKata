6bd09f7d28ae0d579be41f9c60bb59d64501a0d1
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-31 18:46:22 -0400

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,17 @@
+sum = function (numbers) {
+  "use strict";
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+};
+
+//////////////////////
+
+function sum(numbers){
+return numbers.reduce((a,b)=> a+b, 0)
+}
+
+////////////////////
+
