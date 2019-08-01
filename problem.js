f26548aaf1595f49e9795de7c8fcf50398f19bd2
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-31 21:17:12 -0400

    Create problem.js

---
+++ problem.js
@@ -0,0 +1,15 @@
+function problem(x){
+if(typeof x === 'string'){
+return "Error"
+}else{
+return x*50+6
+}
+}
+
+////////////
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+
+//////////////
+
+https://www.codewars.com/kata/super-duper-easy/train/javascript
