c584ef60aa23759cae1732f0199c4b48ab569d76
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-07-31 20:57:59 -0400

    Create repeatStr .js

---
+++ repeatStr .js
@@ -0,0 +1,11 @@
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
+
+/////////////////
+
+repeatStr = (n, s) => s.repeat(n)
+
+/////////////////
+
+https://www.codewars.com/kata/string-repeat/train/javascript
