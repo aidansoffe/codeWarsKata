a850890a831fb6feeb5d8b7d640a360d36e145cd
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-01 21:13:12 -0400

    Create lovefunc.js

---
+++ lovefunc.js
@@ -0,0 +1,17 @@
+function lovefunc(flower1, flower2){
+ if(flower1%2===0 && flower2%2!==0 || flower1%2!==0 && flower2%2===0  ){
+ return true
+ }else{
+ return false
+ }
+}
+
+//////////////////
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+/////////////////
+
+
+https://www.codewars.com/kata/opposites-attract/train/javascript
