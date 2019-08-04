c9ee18b1df77083e13621ad38be8848527604360
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-03 23:45:58 -0400

    Create replaceVowels.js

---
+++ replaceVowels.js
@@ -0,0 +1,7 @@
+function replace(s){
+ return s.replace(/[aeiou]/ig,'!')
+}
+
+///////////////
+
+https://www.codewars.com/kata/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/train/javascript
