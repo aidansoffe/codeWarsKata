567f62d601bda03126b11876881ed536b83f8584
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-04 23:22:03 -0400

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -7,3 +7,10 @@ function cookie(x){
   console.log('!!!!!', x)
     return 'Who ate the last cookie? It was the dog!'}
 }
+
+
+///////////function cookie(x){
+  return typeof x == "string" ? "Who ate the last cookie? It was Zach!" : typeof x == "number" ? "Who ate the last cookie? It was Monica!" : "Who ate the last cookie? It was the dog!";
+}
+
+
