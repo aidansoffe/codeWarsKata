618742e6f65bcf3e2b42df2063bc2259cafd2c79
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-05 20:23:09 -0400

    Create chromosomeCheck.js

---
+++ chromosomeCheck.js
@@ -0,0 +1,24 @@
+function chromosomeCheck(sperm) {
+  if(sperm === 'XX'){
+  return "Congratulations! You're going to have a daughter."
+}else if(sperm === 'XY'){
+  return "Congratulations! You're going to have a son."
+  }
+}
+
+/////////
+
+
+function chromosomeCheck(sperm) {
+if(sperm == "XY")
+{
+  return "Congratulations! You're going to have a son.";
+}
+
+return "Congratulations! You're going to have a daughter.";
+}
+
+////////////
+
+
+https://www.codewars.com/kata/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes/train/javascript
