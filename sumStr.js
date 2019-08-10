21d2557a5ddb5cc6fe0ae438ab6b131971f51c89
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-09 22:01:45 -0400

    Create sumStr.js

---
+++ sumStr.js
@@ -0,0 +1,19 @@
+function sumStr(a,b) {
+var k = +a + +b
+return k.toString()
+}
+
+
+/////////
+
+function sumStr(a,b) {
+  return String(Number(a)+Number(b));
+}
+
+/////////
+
+function sumStr(a,b) {
+  return (+a+ +b)+''  
+}
+
+//////////
