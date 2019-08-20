60df5eadd0c64674788f499ee90ff0c162a6d03b
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-19 22:26:54 -0400

    Create howManyLightsabersDoYouOwn.js

---
+++ howManyLightsabersDoYouOwn.js
@@ -0,0 +1,12 @@
+function howManyLightsabersDoYouOwn(name) {
+  if(name === 'Zach'){
+  return 18
+  }else{
+  return 0}
+}
+
+/////////
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach' ? 18 : 0;
+}
