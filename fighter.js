3e573165b0a4a53acf635ca0dbbab203e0ba4010
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2019-08-20 23:03:30 -0400

    Create fighter.js

---
+++ fighter.js
@@ -0,0 +1,8 @@
+var quote = function(fighter) {
+ fighter=fighter.toLowerCase()
+ if(fighter === 'george saint pierre'){
+ return "I am not impressed by your performance."
+ }else if(fighter === 'conor mcgregor'){
+ return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+ }
+};
