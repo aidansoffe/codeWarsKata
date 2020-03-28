e9b973c9e369aef9408656750cd076b460c841a8
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2020-03-27 23:57:20 -0400

    Update sumStr.js

--- sumStr.js
+++ sumStr.js
@@ -4,13 +4,13 @@ return k.toString()
 }
 
 
-/////////
+///////// opt 2
 
 function sumStr(a,b) {
   return String(Number(a)+Number(b));
 }
 
-/////////
+///////// opt 3
 
 function sumStr(a,b) {
   return (+a+ +b)+''  
